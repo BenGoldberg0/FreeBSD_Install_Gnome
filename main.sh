d21a826cf0174a40098fcaedf43ea4356fb4b2be
Ben <154703839+BenGoldberg0@users.noreply.github.com>
2025-08-27 14:52:13 -0400

    Refactor comments and streamline script execution
    stuff

--- main.sh
+++ main.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
 
-# Exit on error
+# exit if error
 set -e
 
-# Check if running as root
+# check if running as root
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-# Check if running on FreeBSD
 echo "Installing GNOME and other stuff"
 
-# Update package repository
+# update
 pkg update && pkg upgrade -y
 
-# Install XOrg
+# install Xorg
 pkg install -y xorg
 
-# Install GNOME
+# install GNOME
 pkg install -y gnome
 
-# Enable required services in rc.conf
+# enable required services in rc.conf
 sysrc dbus_enable="YES"
 sysrc hald_enable="YES"
 sysrc gdm_enable="YES"
@@ -29,13 +28,13 @@ sysrc gdm_enable="YES"
 # Enable GNOME-related services
 sysrc gnome_enable="YES"
 
-# Configure procfs
+# configure procfs
 if ! mount | grep -q "^procfs"; then
     echo "procfs         /proc           procfs  rw      0       0" >> /etc/fstab
     mount procfs /proc
 fi
 
-# Add required kernel modules to load at boot
+# add required kernel modules that should load at boot
 echo 'linux_enable="YES"' >> /etc/rc.conf
 
 cat >> /boot/loader.conf << EOF
@@ -43,7 +42,7 @@ cuse_load="YES"
 linux_load="YES"
 EOF
 
-# Configure PolicyKit
+# PolicyKit
 mkdir -p /usr/local/etc/polkit-1/rules.d
 cat > /usr/local/etc/polkit-1/rules.d/40-wheel.rules << EOF
 polkit.addRule(function(action, subject) {
@@ -53,9 +52,8 @@ polkit.addRule(function(action, subject) {
 });
 EOF
 
-# Configure .xinitrc for users who want to start GNOME manually
-echo 'exec gnome-session' > /usr/local/etc/X11/xinit/xinitrc
+echo "Installation complete! After reboot, GNOME will start automatically via GDM. Your system will reboot in a little bit".
 
-echo "Installation complete! Please reboot but first read the following:"
-echo "After reboot, GNOME will start automatically via GDM."
-echo "Alternatively, you can start it manually with 'startx' after logging in but there is no point."
+sleep 30
+
+reboot
